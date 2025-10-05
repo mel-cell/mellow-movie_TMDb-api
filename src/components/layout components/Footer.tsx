c70282f94ab59ba890c61bcc38991841dc4e2805
mel-cell <melmel67799@gmail.com>
2025-10-05 15:04:05 +0700

    footer

--- src/components/layout components/Footer.tsx
+++ src/components/layout components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-900 text-white p-4">
       <div className="container mx-auto text-center">
-        <p>&copy; 2023 Mellow Movie. Powered by TMDb API.</p>
+        <p>&copy; 2023 Flexworld Movie. Powered by TMDb API.</p>
       </div>
     </footer>
   );
